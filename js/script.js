73816d8d09c9dd7346cf1b8ec5b5849d74f96d89
Marcin Błażejewski <macin@blazejew.ski>
2022-05-17 21:35:22 +0200

    change - Math.random 19

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+var randomNumber;
+var randomNumber19;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+randomNumber19 = Math.floor(Math.random() * 9 + 11);
+printMessage('Wylosowana liczba to: ' + randomNumber);
+printMessage('Wylosowana liczba dodatkowa to: ' + randomNumber19);
+
+
 var computerMove;
 computerMove = 'papier';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+
