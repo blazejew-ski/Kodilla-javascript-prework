cd6110ec97ef9be26b1f257ade0cac9179c3f0bd
Marcin Błażejewski <macin@blazejew.ski>
2022-05-23 18:14:44 +0200

    added some css styling

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-let PlayerPoints = 0;
-let ComputerPoints = 0;
+let playerPoints = 0;
+let computerPoints = 0;
 
 const results = document.getElementById('results'), buttonRock = document.getElementById('button-rock'), buttonPaper = document.getElementById('button-paper'), buttonScissors = document.getElementById('button-scissors');
 
@@ -29,27 +29,27 @@ function buttonClicked(argButtonName,computerMove, playerMove, randomNumber) {
     console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
     if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
         printMessage('Wygrywasz!');
-        PlayerPoints = PlayerPoints+1;
+        playerPoints = playerPoints+1;
     } 
     else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
         printMessage('Wygrywasz!');
-        PlayerPoints = PlayerPoints+1;
+        playerPoints = playerPoints+1;
     }   
     else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
         printMessage('Wygrywasz!');
-        PlayerPoints = PlayerPoints+1;
+        playerPoints = playerPoints+1;
     }   
     else if (argPlayerMove == argComputerMove) {
         printMessage('Remis!');
     }
     else {
         printMessage('Przegrywasz :(');
-        ComputerPoints = ComputerPoints+1;
+        computerPoints = computerPoints+1;
     }
-    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove + '.');
     
-    console.log('Punkty gracza: ' + PlayerPoints);
-    console.log('Punkty kąkutera: ' + ComputerPoints);
+    console.log('Punkty gracza: ' + playerPoints);
+    console.log('Punkty kąkutera: ' + computerPoints);
     }
     playerMove = argButtonName;
     console.log('ruch gracza to: ' + playerMove);
@@ -57,8 +57,8 @@ function buttonClicked(argButtonName,computerMove, playerMove, randomNumber) {
     console.log('wylosowana liczba to: ' + randomNumber);
     computerMove = getMoveName(randomNumber);
     console.log('ruch komputera to: ' + computerMove);
-    displayResult(playerMove, computerMove, PlayerPoints, ComputerPoints);
-    results.innerHTML = 'Punkty gracza: ' + PlayerPoints + ' | Punkty komputera: ' + ComputerPoints;
+    displayResult(playerMove, computerMove, playerPoints, computerPoints);
+    results.innerHTML = '<div class="pkt-g"><h3>Punkty<br/>gracza:</h3><h5>' + playerPoints + '</h5></div><div class="pkt-k"><h3>Punkty<br/>komputera:</h3><h5>' + computerPoints + '</h5></div>';
 }
 
 buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
